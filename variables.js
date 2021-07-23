63d5604e769a07fc375c6fcffd4f317d0c41cdb0
Carly Collums <carlycollums@gmail.com>
2021-07-23 10:29:41 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = 'Megan Fox'
+var isActive = true
+famousPerson = 'Machine Gun Kelly'
+var num3 = 4
+var num9 = 10
+var sum = num3 + num9
+var animals = ['tiger', 'lion', 'bear']
+array.splice(array.indexOf('tiger'), 1);
+famousPerson.length
