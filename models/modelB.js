52d041d751aa001c3df507def60576b007d61812
Carly Collums <carlycollums@gmail.com>
2021-07-23 10:44:37 -0600

    Model a Vehicle Object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  make: 'Mercedes',
+  model: 'G-Class',
+  year: 2021,
+  milesPerGallon: 30,
+  isUsed: false,
+  isExpensive: true,
+  seatColorOptions: ['black', 'tan', 'red'],
+  exteriorColorOptions: ['grey', 'black', 'white']
+}
