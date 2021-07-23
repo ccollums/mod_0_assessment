08c13f0f67d9d06bd6dfdcc4a79041e9c7aa6843
Carly Collums <carlycollums@gmail.com>
2021-07-23 10:38:06 -0600

    MModel an airport object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: 'DIA',
+  location: 'Denver',
+  numberOfplanes: 500,
+  numberOfTerminals: 100,
+  isInternational: true,
+  isActive: true,
+  airlines: ['United', 'American', 'Frontier'],
+  restaurants: ['Shake Shack', 'Mcdonalds', 'Chick Fil A']
+}
